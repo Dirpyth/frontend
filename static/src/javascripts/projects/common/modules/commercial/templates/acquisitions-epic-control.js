438034765ff3e29a39487b91f3d685a6eceb7e93
Joe Griffiths <joe.griffiths.freelancer@guardian.co.uk>
2020-01-21 11:21:18 +0000

    linter

--- static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-control.js
+++ static/src/javascripts/projects/common/modules/commercial/templates/acquisitions-epic-control.js
@@ -1,7 +1,7 @@
 // @flow
 import { appendToLastElement } from 'lib/array-utils';
 import { acquisitionsEpicTickerTemplate } from 'common/modules/commercial/templates/acquisitions-epic-ticker';
-import { acquisitionsEpicReminderTemplate } from "common/modules/commercial/templates/acquisitions-epic-reminder";
+import { acquisitionsEpicReminderTemplate } from 'common/modules/commercial/templates/acquisitions-epic-reminder';
 
 const buildFooter = (footer: string[]): string =>
     `<div class="contributions__epic-footer">
